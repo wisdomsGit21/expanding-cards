9fb0150a62b45b5a38625190b16a087e97bfc131
Wisdom Ekeh <89356694+wisdomsGit21@users.noreply.github.com>
2023-02-17 09:55:17 +0100

    updated few things

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
-  panel.addEventListener("click", () => {
+  panel.addEventListener("mouseover", () => {
     removeActiveClasses();
     panel.classList.add("active");
   });
